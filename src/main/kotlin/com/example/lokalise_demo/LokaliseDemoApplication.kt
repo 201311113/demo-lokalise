7caa80f56867117f68d0df017714e8a2b0c5355d
201311113 <antjdlekd123@naver.com>
2025-02-18 13:37:49 +0900

    first

---
+++ src/main/kotlin/com/example/lokalise_demo/LokaliseDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.lokalise_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LokaliseDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<LokaliseDemoApplication>(*args)
+}
